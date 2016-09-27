ddfd24494ef0fcd3abcdb2b11679a44780e64359
soezdemir <sadri.oezdemir@googlemail.com>
2016-09-27 17:07:34 +0200

    [+] Destination point along great-circle given distance and bearing from start point

--- src/GeoCalculation.java
+++ src/GeoCalculation.java
@@ -1,8 +1,3 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-import java.math.*;
-
 /** <br>Berechnung einer Distanz mit Geo Koordinaten</br>
  * @author soezdemir
  * @date 2016-09-27
@@ -10,7 +5,7 @@ import java.math.*;
  */
 public class GeoCalculation {
 
-    public static final double EARTH_RAD = 6378.388;
+    public static final double EARTH_RAD = 6378.137;
     //public static final double TEN_MILLION = 10000000.0;
 
     public static void main(String args[]) {
@@ -27,11 +22,12 @@ public class GeoCalculation {
 
         double lat1 = 52.51640; //Berlin
         double lon1 = 13.37770;
-        double lat2 =  38.69267; //Lissabon
+        double lat2 = 38.69267; //Lissabon
         double lon2 = -9.177944;
 
         double distance;
         double largeArc;
+        double bearing;
 
         double radLatitude1 = Math.toRadians(latitude1);
         double radLongitude1 = Math.toRadians(longitude1);
@@ -61,13 +57,19 @@ public class GeoCalculation {
                     Math.cos(radLat1) * Math.cos(radLat2) * Math.cos(radLon2 - radLon1);
         distance =  EARTH_RAD * Math.acos(largeArc);
 
-        System.out.println(" Distance = " + distance + " km\n");
+        bearing = Math.atan2(Math.sin(radLon2 - radLon1) * Math.cos(radLat2),
+                (Math.cos(radLat1) * Math.sin(radLat2)) - (Math.sin(radLat1) * Math.cos(radLat2)) *
+                        Math.cos(radLon2 - radLon1));
+
+
+        System.out.println(" Distance = " + distance + " km");
+        System.out.println(" Bearing = " + bearing + "°\n");
         System.out.println("=======================================================\n");
 
 
-        lat1 = 49.24597; //Altenkesseler Str. [A]
+        lat1 = 49.24597; //Altenkesseler Str. [A] 49.24597, 6.936992
         lon1 = 6.936992;
-        lat2 = 49.24596; //Altenkesseler Str. [B]
+        lat2 = 49.24596; //Altenkesseler Str. [B] 49.24596, 6.935801
         lon2 = 6.935801;
 
         System.out.println(" Altenkesseler Str. [A]\t LAT: " + lat1 + " LON: " + lon1);
@@ -85,12 +87,47 @@ public class GeoCalculation {
 
         System.out.print(" Distance = " + distance * 1000 + " m\n");
 
-        double bearing = Math.atan2(Math.sin(radLon2 - radLon1) * Math.cos(radLat2),
+        bearing = Math.atan2(Math.sin(radLon2 - radLon1) * Math.cos(radLat2),
                          (Math.cos(radLat1) * Math.sin(radLat2)) - (Math.sin(radLat1) * Math.cos(radLat2)) *
                          Math.cos(radLon2 - radLon1));
-        System.out.println(" Bearing = " + bearing + "°\n");
+        //ToDo in Grad umrechnen!!!
+        System.out.println(" Bearing = " + bearing + "\n");
+
+
+        searchPoint(); //Funktionsaufruf
 
 
     }
+
+    public static void searchPoint(){ //ToDo LAT(x) wird noch falsch berechnet
+        double latitude = 49.24597;
+        double longitude = 6.936992;
+        double bearing = 270;
+        double distance = 0.1;
+
+
+        double x = Math.asin(Math.sin(latitude) * Math.cos(distance/EARTH_RAD)
+                    + Math.cos(latitude) * Math.sin(distance/EARTH_RAD) * Math.cos(Math.toRadians(bearing)));
+
+        double y = longitude + Math.atan2(Math.sin(Math.toRadians(bearing))
+                    * Math.sin(distance/EARTH_RAD) * Math.cos(latitude),
+                    Math.cos(distance/EARTH_RAD) - Math.sin(latitude) * Math.sin(x));
+
+        System.out.println(" ===================================================================");
+        System.out.println("\tLAT     : " + latitude + "\tLON: " + longitude);
+        System.out.println("\tBearing : " + bearing);
+        System.out.println("\tDistance: " + distance);
+        System.out.println(" ===================================================================");
+        System.out.println("\tLAT(x): " + x + "  |  LON(y): " + y);
+
+    }
 }
+
+
+
+
+
+//"Der Wissenschaftler ist ein Mann, der lieber zaehlt als vermutet."
+
 //http://www.movable-type.co.uk/scripts/latlong.html
+//http://www.purplemath.com/modules/radians.htm
