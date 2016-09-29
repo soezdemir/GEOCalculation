eb685491c2162d5192e73d5240d0d7b0fa37923d
soezdemir <sadri.oezdemir@googlemail.com>
2016-09-29 19:13:09 +0200

    [+] searchPoint() implementiert

--- src/GeoCalculation.java
+++ src/GeoCalculation.java
@@ -9,25 +9,20 @@ public class GeoCalculation {
 
     //public static final double TEN_MILLION = 10000000.0;
 
-
-    public double distance;
-    public double bearing;
-
-    public WGS84Point pointA;
-    public WGS84Point pointB;
-
-
     /**
      * Standard constructor for GeoCalculation
      */
     public GeoCalculation(){}
 
-
+    /**
+     *
+     * @param pointA
+     * @param pointB
+     * @return
+     */
     public static double getDistanceBetween(WGS84Point pointA, WGS84Point pointB)
     {
         double largeArc;
-        double distance;
-
         double radLatitude1 = Math.toRadians(pointA.getLatitude());
         double radLongitude1 = Math.toRadians(pointA.getLongitude());
         double radLatitude2 = Math.toRadians(pointB.getLatitude());
@@ -37,90 +32,40 @@ public class GeoCalculation {
                   Math.cos(radLatitude1) * Math.cos(radLatitude2) *
                         Math.cos(radLongitude2 - radLongitude1);
 
-        distance = EARTH_RAD * Math.acos(largeArc);
-
-        return distance;
+        return EARTH_RAD * Math.acos(largeArc);
     }
 
-    @Override
-    public String toString() {
-
-        String result = " Distance\t=> " + distance + "\n" +
-                        " Bearing\t=> " + bearing + "\n";
-        return result;
-    }
-
-    //ToDo =========================================================================================================
-    public static void searchParkingSlot(){
-
-        double lat1 = 49.245922; //Altenkesseler Str. [A]
-        double lon1 = 6.935884;
-        double lat2 = 49.245943; //Altenkesseler Str. [B]
-        double lon2 = 6.936975;
-        System.out.println(" Parking Slot:");
-        System.out.println(" Altenkesseler Str. [A]\t LAT: " + lat1 + " LON: " + lon1);
-        System.out.println(" Altenkesseler Str. [B]\t LAT: " + lat2 + " LON: " + lon2);
-
-        double radLat1 = Math.toRadians(lat1);
-        double radLon1 = Math.toRadians(lon1);
-        double radLat2 = Math.toRadians(lat2);
-        double radLon2 = Math.toRadians(lon2);
-
-        double largeArc =  Math.sin(radLat1) * Math.sin(radLat2) +
-                Math.cos(radLat1) * Math.cos(radLat2) * Math.cos(radLon2 - radLon1);
-
-        double distance =  EARTH_RAD * Math.acos(largeArc);
+    /**
+     *
+     * @param pointA
+     * @param pointB
+     * @return
+     */
+    public static double getBearing(WGS84Point pointA, WGS84Point pointB)
+    {
+        double radLat1 = Math.toRadians(pointA.getLatitude());
+        double radLon1 = Math.toRadians(pointA.getLongitude());
+        double radLat2 = Math.toRadians(pointB.getLatitude());
+        double radLon2 = Math.toRadians(pointB.getLongitude());
 
         double bearing = Math.atan2(Math.sin(radLon2 - radLon1) * Math.cos(radLat2),
                 (Math.cos(radLat1) * Math.sin(radLat2)) - (Math.sin(radLat1) * Math.cos(radLat2)) *
-                        Math.cos(radLon2 - radLon1));
-
-        System.out.println(" Distance => " + distance * 1000 + " m");
-        System.out.println(" Bearing  => " + (Math.toDegrees(bearing)+360)%360 + "\n");
+                 Math.cos(radLon2 - radLon1));
+        return Math.toDegrees(bearing);
     }
 
-
-    //ToDo =========================================================================================================
-    public static void distanceBetweenCities(){
-
-        double lat1 = 52.51640; //Berlin
-        double lon1 = 13.37770;
-        double lat2 = 38.69267; //Lissabon
-        double lon2 = -9.177944;
-        double largeArc, distance, bearing;
-        double radLat1 = Math.toRadians(lat1);
-        double radLon1 = Math.toRadians(lon1);
-        double radLat2 = Math.toRadians(lat2);
-        double radLon2 = Math.toRadians(lon2);
-
-        largeArc =  Math.sin(radLat1) * Math.sin(radLat2) +
-                Math.cos(radLat1) * Math.cos(radLat2) * Math.cos(radLon2 - radLon1);
-        distance =  EARTH_RAD * Math.acos(largeArc);
-
-        bearing = Math.atan2(Math.sin(radLon2 - radLon1) * Math.cos(radLat2),
-                (Math.cos(radLat1) * Math.sin(radLat2)) - (Math.sin(radLat1) * Math.cos(radLat2)) *
-                        Math.cos(radLon2 - radLon1));
-
-        System.out.println(" Berlin   =>\tLAT: " + lat1 + "\tLON: " + lon1);
-        System.out.println(" Lissabon =>\tLAT: " + lat2 + "\tLON: " + lon2);
-        System.out.println(" Distance => " + distance + " km");
-        System.out.println(" Bearing  => " + (Math.toDegrees(bearing)+360)%360 + "°\n");
-        System.out.println("=======================================================\n");
-    }
-    //ToDo =============================================================================================================
-
-
-    //ToDo =============================================================================================================
-    public static void searchPoint(){
-        double latitude     = 49.245922;            //x = 49.245922
-        double longitude    = 6.935884;             //y =  6.935884
-        double bearing      = 88.31069344488208;    //Peilungswinkel
-        double distance     = 0.07922964485129694;  //Distanz
-
-        bearing = Math.toRadians(bearing);
-        latitude = Math.toRadians(latitude);
-        longitude = Math.toRadians(longitude);
-
+    /**
+     *
+     * @param point
+     * @param distance
+     * @param angle
+     * @return
+     */
+    public static WGS84Point searchPoint(WGS84Point point, double distance, double angle)
+    {
+        double bearing = Math.toRadians(angle);
+        double latitude = Math.toRadians(point.getLatitude());
+        double longitude = Math.toRadians(point.getLongitude());
 
         double x = Math.asin(Math.sin(latitude) * Math.cos(distance/EARTH_RAD)
                     + Math.cos(latitude) * Math.sin(distance/EARTH_RAD) * Math.cos(bearing));
@@ -128,32 +73,14 @@ public class GeoCalculation {
         double y = longitude + Math.atan2(Math.sin(bearing) * Math.sin(distance/EARTH_RAD) * Math.cos(latitude),
                     Math.cos(distance/EARTH_RAD) - Math.sin(latitude) * Math.sin(x));
 
-        //y = (((y + 540) % 360) - 180 );
-
-        double finalBearing = (bearing + 180) % 360;
-
-        latitude = Math.toDegrees(latitude);
-        longitude = Math.toDegrees(longitude);
-        bearing = Math.toDegrees(bearing);
-
         x = Math.toDegrees(x);
         y = Math.toDegrees(y);
 
-        System.out.println("===================================================================");
-        System.out.println(" searchPoint()");
-        System.out.println("\tLAT     : " + latitude + "\t|\tLON: " + longitude);
-        System.out.println("\tBearing : " + bearing);
-        System.out.println("\tDistance: " + (distance * 1000) + " m");
-        System.out.println("___________________________________________________________________");
-        System.out.println("\tLAT(x): " + x + "\t|\tLON(y): " + y + "\n\tFinal Bearing: " + finalBearing);
-
+        return new WGS84Point(x,y);
     }
-    //ToDo =============================================================================================================
 
-
+    //ToDo schnittpunkt zweier geraden um Rechteck aufzuspannen
 }
-
-
 
 //"Der Wissenschaftler ist ein Mann, der lieber zaehlt als vermutet."
 
--- src/GeoStart.java
+++ src/GeoStart.java
@@ -21,39 +21,44 @@ public class GeoStart {
     {
         WGS84Point a = new WGS84Point(49.9917, 8.41321);  //Ruesselsheim -
         WGS84Point b = new WGS84Point(50.0049, 8.42182);  //Ruesselsheim -
-
-        //GeoCalculation.init();
         System.out.println(a);
         System.out.println(b);
-        System.out.println("\tDistance:\t" + GeoCalculation.getDistanceBetween(a, b) + " km\n");
+        System.out.println("\tDistance:\t" + GeoCalculation.getDistanceBetween(a, b) + " km");
+        System.out.println("\tBearing:\t" + GeoCalculation.getBearing(a, b) + "°\n");
 
         WGS84Point berlin = new WGS84Point(52.51640, 13.37770);  //Berlin
         WGS84Point lissab = new WGS84Point(38.69267, -9.177944); //Lissabon
         System.out.println(berlin);
         System.out.println(lissab);
-        System.out.println("\tDistance:\t" + GeoCalculation.getDistanceBetween(berlin, lissab) + " km\n");
+        System.out.println("\tDistance:\t" + GeoCalculation.getDistanceBetween(berlin, lissab) + " km");
+        System.out.println("\tBearing:\t" + GeoCalculation.getBearing(berlin, lissab) + "°\n");
 
         WGS84Point c = new WGS84Point(49.245922, 6.935884);
         WGS84Point d = new WGS84Point(49.245943, 6.936975);
         System.out.println(c);
         System.out.println(d);
-        System.out.println("\tDistance:\t" + GeoCalculation.getDistanceBetween(c, d) + " km\n");
+        System.out.println("\tDistance:\t" + GeoCalculation.getDistanceBetween(c, d) + " km");
+        System.out.println("\tBearing:\t" + GeoCalculation.getBearing(c, d) + "°\n");
 
         WGS84Point e = new WGS84Point(49.198811, 6.693405); //Rue de la Bonne Fontaine
         WGS84Point f = new WGS84Point(49.199084, 6.692552); //Rue de la Bonne Fontaine
         System.out.println(e);
         System.out.println(f);
-        System.out.println("\tDistance:\t" + GeoCalculation.getDistanceBetween(e, f) + " km\n");
+        System.out.println("\tDistance:\t" + GeoCalculation.getDistanceBetween(e, f) + " km");
+        System.out.println("\tBearing:\t" + GeoCalculation.getBearing(e, f) + "°\n");
 
         WGS84Point frankf = new WGS84Point(50.039258, 8.562203); //Frankfurt Airport
         WGS84Point istanb = new WGS84Point(40.983693, 28.810560); //Istanbul Atatuerk Airport
         System.out.println(frankf);
         System.out.println(istanb);
-        System.out.println("\tDistance:\t" + GeoCalculation.getDistanceBetween(frankf, istanb) + " km\n");
+        System.out.println("\tDistance:\t" + GeoCalculation.getDistanceBetween(frankf, istanb) + " km");
+        System.out.println("\tBearing:\t" + GeoCalculation.getBearing(frankf, istanb) + "°\n");
 
-        GeoCalculation.searchPoint();
 
-
+        double distance = 0.07922964485129694;
+        double bearing = 88.31069344488208;
+        WGS84Point q = new WGS84Point(49.245922, 6.935884);
+        System.out.println(" searchPoint() --> " + GeoCalculation.searchPoint(q, distance, bearing));
         //GeoCalculation.distanceBetweenCities();
         //GeoCalculation.searchParkingSlot();
         //testValuesForDistanceBetween();
@@ -65,7 +70,7 @@ public class GeoStart {
 
     /**
      * Parameters to testing GeoCalculation
-     * @param WGS84Point (49.245078, 6.936198) with latitude and longitude values
+     * (49.245078, 6.936198) with latitude and longitude values
      */
     public static void testValuesForDistanceBetween(){
 
